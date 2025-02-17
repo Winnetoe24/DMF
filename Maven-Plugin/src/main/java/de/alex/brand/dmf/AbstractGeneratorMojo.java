b5499c50a4cad7e31f02dcb47de52815842c2423
Winnetoe <Alexander.Brand@unitybox.de>
2025-02-17 17:14:09 +0100

    Add AbstractGeneratorMojo

---
+++ Maven-Plugin/src/main/java/de/alex/brand/dmf/AbstractGeneratorMojo.java
@@ -0,0 +1,105 @@
+package de.alex.brand.dmf;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.Scanner;
+
+public abstract class AbstractGeneratorMojo extends AbstractMojo {
+    @Parameter(defaultValue = "./target/generated-sources/dmf/")
+    public String directory;
+
+    @Parameter(defaultValue = "./target/dmf/install")
+    public String installDirectory;
+
+    @Parameter(defaultValue = "${project}")
+    private MavenProject project;
+
+    @Parameter
+    public String modelPath;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        Log log = getLog();
+        log.info("Preparing Generator");
+        File generatorFile = prepareGenerator();
+
+        project.addCompileSourceRoot(directory);
+        generate(log, generatorFile);
+
+    }
+
+    protected File prepareGenerator() throws MojoExecutionException, MojoFailureException {
+        File file = new File(getGeneratorUsePath());
+        if (file.exists()) {
+            return file;
+        }
+        file.getParentFile().mkdirs();
+        try {
+            InputStream resource = this.getClass().getResourceAsStream("/" + getGeneratorName());
+            Files.copy(resource, file.toPath());
+        } catch (IOException e) {
+            throw new MojoFailureException("Can't find Generator to copy", e);
+        }
+        return file;
+    }
+
+    protected String getGeneratorUsePath() {
+        if (installDirectory == null) {
+            installDirectory = "./";
+        }
+        if (!installDirectory.endsWith("/")) {
+            installDirectory += "/";
+        }
+        return installDirectory + getGeneratorName();
+    }
+
+    protected String getGeneratorName() {
+        if (OperatingSystem.isUnix()) {
+            return "javaGenerator";
+        }
+        if (OperatingSystem.isWindows()) {
+            return "javaGenerator.exe";
+        }
+        throw new RuntimeException("Unsupported Operating System");
+    }
+
+    protected abstract void generate(Log log, File generatorFile) throws MojoExecutionException, MojoFailureException;
+
+    protected enum LogMode {
+        INFO,
+        ERROR,
+    }
+
+    protected MojoExecutionException processOutput(Log log, Process process) {
+        MojoExecutionException mojoExecutionException = null;
+        try (Scanner scanner = new Scanner(process.getInputStream())) {
+            GenerateModelMojo.LogMode mode = GenerateModelMojo.LogMode.INFO;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.startsWith("panic:")) {
+                    mode = GenerateModelMojo.LogMode.ERROR;
+                    mojoExecutionException = new MojoExecutionException("Es kam zu einem Fehler bei der Generation:" + line.substring("panic:".length()));
+                } else if (line.startsWith("Generate")) {
+                    mode = GenerateModelMojo.LogMode.INFO;
+                }
+                if (mode == LogMode.INFO) {
+                    log.info(line);
+                } else {
+                    log.error(line);
+                }
+
+            }
+        }
+        return mojoExecutionException;
+    }
+
+}
--- Maven-Plugin/src/main/java/de/alex/brand/dmf/GenerateModelMojo.java
+++ Maven-Plugin/src/main/java/de/alex/brand/dmf/GenerateModelMojo.java
@@ -1,58 +1,40 @@
 package de.alex.brand.dmf;
 
-import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.util.Scanner;
 
 @Mojo(name = "generate-model", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
-public class GenerateModelMojo extends AbstractMojo {
+public class GenerateModelMojo extends AbstractGeneratorMojo {
 
-    @Parameter(defaultValue = "./src/gen/java/")
-    public String directory;
-
-    @Parameter(defaultValue = "./target/dmf/install")
-    public String installDirectory;
-
-    @Override
-    public void execute() throws MojoExecutionException, MojoFailureException {
-        Log log = getLog();
-        log.info("Preparing Generator");
-        File generatorFile = prepareGenerator();
-
-        generate(log, generatorFile);
-
-    }
 
     protected void generate(Log log, File generatorFile) throws MojoExecutionException, MojoFailureException {
         log.info("Generating Model into " + directory);
-        ProcessBuilder processBuilder = new ProcessBuilder(generatorFile.getAbsolutePath(), "--help");
+        ProcessBuilder processBuilder = new ProcessBuilder(generatorFile.getAbsolutePath(),
+                "--basePath", directory);
 
+        if (modelPath != null) {
+            processBuilder.command().add("--modelPath");
+            processBuilder.command().add(modelPath);
+        }
         processBuilder.redirectErrorStream(true);
-//        processBuilder.inheritIO();
 
         try {
             Process process = processBuilder.start();
-
-            try (Scanner scanner = new Scanner(process.getInputStream())) {
-                log.info("Read input stream");
-                while (scanner.hasNextLine()) {
-                    log.info(scanner.nextLine());
-                }
-            }
+            MojoExecutionException mojoExecutionException = processOutput(log, process);
             int exitCode = process.waitFor();
             log.info("Generator exited with code " + exitCode);
+            if (mojoExecutionException != null) {
+                throw mojoExecutionException;
+            }
+            if (exitCode != 0) {
+                throw new MojoFailureException("Generation failed, because of wrong exit code: " + exitCode);
+            }
         } catch (IOException | InterruptedException e) {
             throw new MojoFailureException(e);
         }
@@ -60,32 +42,5 @@ public class GenerateModelMojo extends AbstractMojo {
 
     }
 
-    protected File prepareGenerator() throws MojoExecutionException, MojoFailureException {
-        File file = new File(getGeneratorUsePath());
-        if (file.exists()) {
-            return file;
-        }
-        file.getParentFile().mkdirs();
-        try {
-            InputStream resource = this.getClass().getResourceAsStream("/" + getGeneratorName());
-            Files.copy(resource, file.toPath());
-        } catch (IOException e) {
-            throw new MojoFailureException("Can't find Generator to copy",e);
-        }
-        return file;
-    }
 
-    protected String getGeneratorUsePath() {
-        if (installDirectory == null) {
-            installDirectory = "./";
-        }
-        if (!installDirectory.endsWith("/")) {
-            installDirectory += "/";
-        }
-        return installDirectory+getGeneratorName();
-    }
-
-    protected String getGeneratorName() {
-        return "javaGenerator.exe";
-    }
 }
