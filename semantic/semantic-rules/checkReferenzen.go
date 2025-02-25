36c883205193d3e65097e114a2e4655608fb89bb
Winnetoe <Alexander.Brand@unitybox.de>
2025-02-25 16:39:38 +0100

    Semantik fast fertig

--- semantic/semantic-rules/checkReferenzen.go
+++ semantic/semantic-rules/checkReferenzen.go
@@ -31,6 +31,24 @@ func (c *checkReferenzen) handleStruct(element *packages.StructElement, _ *packa
 		}
 	}
 
+	for _, referenz := range element.MultiReferenzen {
+		if referenz.Generics[0].ModelPath != nil {
+			typString := referenz.Generics[0].ModelPath.ToString()
+			_, found := (*c.lookup)[typString]
+			if !found {
+				c.elements = append(c.elements, errElement.CreateErrorElementCxt(referenz.Node.Child(1), errors.New("Referenzierter Typ nicht gefunden!"), referenz.Node))
+			}
+		}
+
+		if referenz.Generics[1].ModelPath != nil {
+			typString := referenz.Generics[1].ModelPath.ToString()
+			_, found := (*c.lookup)[typString]
+			if !found {
+				c.elements = append(c.elements, errElement.CreateErrorElementCxt(referenz.Node.Child(1), errors.New("Referenzierter Typ nicht gefunden!"), referenz.Node))
+			}
+		}
+	}
+
 	for _, funktion := range element.Funktionen {
 		for _, variable := range funktion.Parameter {
 			variablePath, _, _ := variable.GetVariableType()
@@ -41,6 +59,16 @@ func (c *checkReferenzen) handleStruct(element *packages.StructElement, _ *packa
 					referenz := variable.(*packages.Referenz)
 					c.elements = append(c.elements, errElement.CreateErrorElementCxt(referenz.Node.Child(1), errors.New("Referenzierter Typ des Parameter nicht gefunden!"), referenz.Node))
 				}
+			}
+		}
+		returnType := funktion.ReturnType
+		variablePath, _, _ := returnType.GetVariableType()
+		if variablePath != nil {
+			variableString := variablePath.ToString()
+			_, found := (*c.lookup)[variableString]
+			if !found {
+				referenz := returnType.(*packages.Referenz)
+				c.elements = append(c.elements, errElement.CreateErrorElementCxt(referenz.Node.Child(1), errors.New("Referenzierter Typ des Return Typs nicht gefunden!"), referenz.Node))
 			}
 		}
 	}
@@ -57,6 +85,16 @@ func (c *checkReferenzen) handleInterface(element *packages.InterfaceElement) {
 					referenz := variable.(*packages.Referenz)
 					c.elements = append(c.elements, errElement.CreateErrorElementCxt(referenz.Node, errors.New("Referenzierter Typ des Parameter nicht gefunden!"), funktion.Node))
 				}
+			}
+		}
+		returnType := funktion.ReturnType
+		variablePath, _, _ := returnType.GetVariableType()
+		if variablePath != nil {
+			variableString := variablePath.ToString()
+			_, found := (*c.lookup)[variableString]
+			if !found {
+				referenz := returnType.(*packages.Referenz)
+				c.elements = append(c.elements, errElement.CreateErrorElementCxt(referenz.Node.Child(1), errors.New("Referenzierter Typ des Return Typs nicht gefunden!"), referenz.Node))
 			}
 		}
 	}
